62dd7d25e789a05245782714719c1289aa30c83d
Cleber Pacheco <cpacheco@MacMini.local>
2018-07-20 12:04:57 -0300

    Added general structure and init styles and functions.

--- app/assets/js/application.js
+++ app/assets/js/application.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-  
+  $('.product-quantity i').on('click', function() {
+    var qtd = parseInt($(this).parent().attr('data-quantity')),
+        price = parseInt($(this).closest('.product-list__item').find('.product-value').attr('data-price'));
+
+    if($(this).hasClass('fa-plus')) {
+      qtd += 1;
+
+      $(this).parent().attr('data-quantity', qtd);
+    } else if($(this).hasClass('fa-minus')) {
+      if(qtd > 1) {
+        qtd -= 1;
+
+        $(this).parent().attr('data-quantity', qtd);
+      }
+    }
+
+    $(this).parent().find('p').html(qtd + ' unid.');
+    $(this).closest('.product-list__item').find('.product-value strong').html('R$ ' + qtd*price + ',00');
+  });
 });
