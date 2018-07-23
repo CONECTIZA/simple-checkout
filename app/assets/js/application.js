87750267ad368a2bf1dca3a18ec82223adbcb315
Cleber <hello@cleberpacheco.com>
2018-07-23 17:06:51 -0300

    Added functions.

--- app/assets/js/application.js
+++ app/assets/js/application.js
@@ -32,5 +32,62 @@ $(document).ready(function() {
     updateTotal();
   });
 
+  $('input[name="email"]').keypress(function() {
+    var _this = this;
+
+    setTimeout(function() {
+      if($(_this).is(':valid')) {
+        $('.form-group--email h1').html('Clique em prosseguir');
+        $('.btn-finish').hide();
+        $('.btn-prosseguir').show();
+        $(this).removeClass('is-invalid').addClass('.is-valod');
+        $('.login-social-bottom').hide();
+      } else {
+        $(this).addClass('is-invalid');
+      }
+    }, 1000);
+  });
+
+  $('.btn-prosseguir').on('click', function() {
+    $('input[name="email"]').attr('disabled', 'true');
+    $(this).hide();
+    $('.form-group--email h1').html('Validando e-mail...');
+    setTimeout(function() {
+      $('.form-group--email h1').html('Validando e-mail... Concluído.');
+      $('.user-email').html($('input[name="email"]').val());
+      $('.form-group--login .btn-finish').show();
+
+      setTimeout(function() {
+        $('.form-group--email').hide();
+        $('.form-group--login').show();
+      }, 1000);
+    }, 2000);
+  });
+
+  $('.form-group--login .btn-finish').on('click', function() {
+    if($('input[name="password-login"]').val().length > 0) {
+      $('.form-group--login').hide();
+      $('.form-group--nova-conta').show();
+    }
+  });
+
+  $('input[name="cep"]').keypress(function() {
+    var _this = this;
+
+    setTimeout(function() {
+      var val = $(_this).val();
+
+      $.ajax({
+        url: 'https://viacep.com.br/ws/'+ val +'/json',
+        Accept: "text/plain; charset=utf-8",         
+                "Content-Type": "text/plain; charset=utf-8"
+      }).done(function(data) {
+        $('input[name="logadouro"]').val(data.logradouro).attr('disabled');
+        $('input[name="bairro"]').val(data.bairro).attr('disabled');
+        $('input[name="estado"]').val(data.uf).attr('disabled');
+      });
+    }, 1000);
+  });
+
   updateTotal();
 });
